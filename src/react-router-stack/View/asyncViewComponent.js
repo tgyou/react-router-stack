2c44f5eb43882be3c1f3057aebd26b70399e5cbc
Taegyu Yoo <styx@linecorp.com>
2021-09-06 12:41:27 +0900

    asyncViewComponent using React.lazy instead of react-async-component

--- src/react-router-stack/View/asyncViewComponent.js
+++ src/react-router-stack/View/asyncViewComponent.js
@@ -1,5 +1,36 @@
-import { asyncComponent } from 'react-async-component';
+import PropTypes from 'prop-types';
+import { Component, lazy, Suspense } from 'react';
 import { View } from './View';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { error, hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // do something if you need to.
+  }
+
+  render() {
+    if (this.state.hasError) {
+      const ErrorComponent = this.props.errorComponent;
+
+      return ErrorComponent ? <ErrorComponent error={this.state.error} /> : null;
+    } else {
+      return this.props.children;
+    }
+  }
+}
+
+ErrorBoundary.propTypes = {
+  errorComponent: PropTypes.func,
+  children: PropTypes.node.isRequired,
+};
 
 export function asyncViewComponent({
   resolve,
@@ -8,15 +39,15 @@ export function asyncViewComponent({
   ErrorComponent = asyncViewComponent.ErrorComponent,
   WrapperComponent = View,
 }) {
-  const Component = asyncComponent({
-    resolve,
-    LoadingComponent,
-    ErrorComponent,
-  });
+  const Component = lazy(resolve);
 
   return props => (
     <WrapperComponent {...componentProps}>
-      <Component {...props} />
+      <ErrorBoundary errorComponent={ErrorComponent || null}>
+        <Suspense fallback={LoadingComponent ? LoadingComponent(componentProps) : null}>
+          <Component {...props} />
+        </Suspense>
+      </ErrorBoundary>
     </WrapperComponent>
   );
 }
